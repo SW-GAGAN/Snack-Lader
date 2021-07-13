cdd5c89378ab4b1775bf396567dad872582b8ed1
SW-GAGAN <srgagan73@gmail.com>
2021-07-13 20:17:49 +0530

    Adding: S&L game playing get dice no

--- SnackLader.java
+++ SnackLader.java
@@ -3,6 +3,9 @@ import java.util.*;
 public class SnackLader {
 	public static void main(String[] args){
 		int start = 0, end = 100, count = 0; 
+		Random rand1 = new Random();
+		int dice = rand1.nextInt(6)+1;
+System.out.println("dice no is" +dice);
 	}
 
 }
